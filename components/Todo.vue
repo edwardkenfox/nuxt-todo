a2b42ee0c1b4672588f5b71da0a8e99da6657644
Edward Fox <edwardkenfox@gmail.com>
2017-06-04 21:55:09 +0900

    Create test Todo component

---
+++ components/Todo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>ToDos</div>
+    <ul>
+      <li v-for="(todo, index) in todos">
+        <input type="checkbox" :checked="todo.done" v-model="todo.done">
+        {{ todo.title }}
+      </li>
+    </ul>
+    <input type="text" v-model="newTodoTitle">
+    <button @click="addTodo">Add ToDo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      todos: [],
+      newTodoTitle: ''
+    }
+  },
+
+  methods: {
+    addTodo () {
+      if (!this.newTodoTitle) return false
+
+      this.todos.push({
+        title: this.newTodoTitle,
+        done: false
+      })
+
+      this.newTodoTitle = ''
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,20 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
       <h1 class="title">
         NUXT
       </h1>
-      <h2 class="subtitle">
-        Universal Vue.js Application
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+      <todo></todo>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~components/Logo.vue'
+import Todo from '~components/Todo.vue'
 
 export default {
   components: {
-    Logo
+    Todo
   }
 }
 </script>
